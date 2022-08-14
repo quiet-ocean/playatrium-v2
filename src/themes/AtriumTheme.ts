712973f1835f6dcf245eef05c3784668d71e63f8
chris bishop <ychrisbishop@gmail.com>
2022-08-14 05:20:48 +0200

    Update typography system of theme

--- src/themes/AtriumTheme.ts
+++ src/themes/AtriumTheme.ts
@@ -33,18 +33,61 @@ const AtriumTheme = createTheme({
     // h1: {
 
     // },
+    h2: {
+      //styleName: Desktop/H2;
+      fontFamily: 'Fractul',
+      fontSize: '72px',
+      fontWeight: 600,
+      lineHeight: '76px',
+      letterSpacing: '-0.03em',
+      textAlign: 'left',
+    },
+    h3: {
+      fontFamily: 'Fractul',
+      fontSize: '48px',
+      fontWeight: 600,
+      lineHeight: '105%',
+      letterSpacing: '-0.03em',
+      textAlign: 'left',
+    },
+    h4: {
+      fontFamily: 'Fractul Alt',
+      fontSize: '36px',
+      fontWeight: 600,
+      lineHeight: '105%',
+      letterSpacing: '-0.03em',
+      textAlign: 'left',
+    },
+    h5: {
+      fontFamily: 'Fractul Alt',
+      fontSize: '24px',
+      fontWeight: 600,
+      lineHeight: '105%',
+      letterSpacing: '-0.03em',
+      textAlign: 'left',
+    },
     // subtitle1: {
 
     // },
     // subtitle2: {
 
     // },
-    // body1: {
-
-    // },
-    // body2: {
-
-    // },
+    body1: {
+      fontFamily: 'Andale Mono Regular',
+      fontSize: '24px',
+      fontWeight: 400,
+      lineHeight: '105%',
+      letterSpacing: '-0.05em',
+      textAlign: 'left',
+    },
+    body2: {
+      fontFamily: 'Andale Mono Regular',
+      fontSize: '28px',
+      fontWeight: 400,
+      lineHeight: '110%',
+      letterSpacing: '-0.05em',
+      textAlign: 'left',
+    },
     // button: {
 
     // },
