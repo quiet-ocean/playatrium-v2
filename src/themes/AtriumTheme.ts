b93089a02e3e46edb4d3fc1088266a18ebecc07e
chris bishop <ychrisbishop@gmail.com>
2022-08-14 05:05:08 +0200

    Create AtriumTheme.ts

---
+++ src/themes/AtriumTheme.ts
@@ -0,0 +1,63 @@
+import { createTheme } from "@mui/material";
+
+const AtriumTheme = createTheme({
+  palette: {
+    background: {
+      default: '',
+      paper: '',
+    },
+    common: {
+      white: '',
+      black: '',
+    },
+    grey: {
+      100: '',
+    },
+    mode: 'dark',
+    primary: {
+      main: '',
+      light: '',
+      dark: '',
+      // contrastText: '',
+    },
+    secondary: {
+      main: '',
+      light: '',
+      dark: ''
+    },
+    text: {
+
+    }
+  },
+  typography: {
+    // h1: {
+
+    // },
+    // subtitle1: {
+
+    // },
+    // subtitle2: {
+
+    // },
+    // body1: {
+
+    // },
+    // body2: {
+
+    // },
+    // button: {
+
+    // },
+    // caption: {
+
+    // },
+    // overline: {
+
+    // }
+  }
+
+})
+
+export default AtriumTheme
+
+export const palette = AtriumTheme.palette
