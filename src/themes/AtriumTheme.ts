c1cdbbb0d767284213037fd3471f0ad847adb029
chris bishop <ychrisbishop@gmail.com>
2022-08-14 13:00:58 +0200

    Fix lint issues

--- src/themes/AtriumTheme.ts
+++ src/themes/AtriumTheme.ts
@@ -1,4 +1,4 @@
-import { createTheme } from "@mui/material";
+import { createTheme } from '@mui/material'
 
 const AtriumTheme = createTheme({
   palette: {
@@ -7,96 +7,97 @@ const AtriumTheme = createTheme({
       paper: '#252525',
     },
     common: {
-      white: '#F2F2F2',
       black: '#1A1A1A',
-    },
-    grey: {
-      100: '',
-    },
-    mode: 'dark',
-    primary: {
-      main: '',
-      light: '',
-      dark: '',
-      // contrastText: '',
-    },
-    secondary: {
-      main: '',
-      light: '',
-      dark: ''
-    },
-    text: {
-      primary: '#F2F2F2',
-      secondary: '#252525',
-      disabled: '#4A4A4A',
+      white: '#F2F2F2',
     },
     error: {
       main: '#D11F00',
     },
+    grey: {
+      100: '',
+    },
     info: {
       main: '#0049D1',
     },
+    mode: 'dark',
+    primary: {
+      dark: '',
+      light: '',
+      main: '',
+      // contrastText: '',
+    },
+    secondary: {
+      dark: '',
+      light: '',
+      main: '',
+    },
+    text: {
+      disabled: '#4A4A4A',
+      primary: '#F2F2F2',
+      secondary: '#252525',
+    },
     warning: {
       main: '#FFDC00',
-    }
+    },
   },
   typography: {
-    // h1: {
+    // subtitle1: {
+    // },
+    // subtitle2: {
+    // },
+    body1: {
+      fontFamily: 'Andale Mono Regular',
+      fontSize: '24px',
+      fontWeight: 400,
+      letterSpacing: '-0.05em',
+      lineHeight: '105%',
+      textAlign: 'left',
+    },
 
+    body2: {
+      fontFamily: 'Andale Mono Regular',
+      fontSize: '28px',
+      fontWeight: 400,
+      letterSpacing: '-0.05em',
+      lineHeight: '110%',
+      textAlign: 'left',
+    },
+
+    // h1: {
     // },
     h2: {
       //styleName: Desktop/H2;
       fontFamily: 'Fractul',
       fontSize: '72px',
       fontWeight: 600,
-      lineHeight: '76px',
       letterSpacing: '-0.03em',
+      lineHeight: '76px',
       textAlign: 'left',
     },
+
     h3: {
       fontFamily: 'Fractul',
       fontSize: '48px',
       fontWeight: 600,
-      lineHeight: '105%',
       letterSpacing: '-0.03em',
+      lineHeight: '105%',
       textAlign: 'left',
     },
+
     h4: {
       fontFamily: 'Fractul Alt',
       fontSize: '36px',
       fontWeight: 600,
-      lineHeight: '105%',
       letterSpacing: '-0.03em',
+      lineHeight: '105%',
       textAlign: 'left',
     },
     h5: {
       fontFamily: 'Fractul Alt',
       fontSize: '24px',
       fontWeight: 600,
-      lineHeight: '105%',
       letterSpacing: '-0.03em',
-      textAlign: 'left',
-    },
-    // subtitle1: {
-
-    // },
-    // subtitle2: {
-
-    // },
-    body1: {
-      fontFamily: 'Andale Mono Regular',
-      fontSize: '24px',
-      fontWeight: 400,
       lineHeight: '105%',
-      letterSpacing: '-0.05em',
-      textAlign: 'left',
-    },
-    body2: {
-      fontFamily: 'Andale Mono Regular',
-      fontSize: '28px',
-      fontWeight: 400,
-      lineHeight: '110%',
-      letterSpacing: '-0.05em',
       textAlign: 'left',
     },
     // button: {
@@ -108,8 +109,7 @@ const AtriumTheme = createTheme({
     // overline: {
 
     // }
-  }
-
+  },
 })
 
 export default AtriumTheme
