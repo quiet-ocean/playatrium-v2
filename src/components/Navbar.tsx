e1419e2a22c5c64d7036f486da0e26dfdf45cedc
chris bishop <ychrisbishop@gmail.com>
2022-08-15 04:37:31 +0200

    Update navbar item strings

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { Box, Button } from '@mui/material'
 
 import { palette } from '../themes/AtriumTheme'
 
-const items = ['news', 'summary', 'integrations', 'profiles', 'team']
+const items = ['updates', 'overview', 'integrations', 'profiles', 'team']
 export const Navbar = () => {
   return (
     <Box display={`flex`} gap={`12px`} p="4px">
