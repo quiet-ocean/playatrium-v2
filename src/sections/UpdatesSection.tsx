17c54a4258f9c97eb1f8305d163c08f4c3f6d011
chris bishop <ychrisbishop@gmail.com>
2022-08-22 03:24:05 +0200

    Update update section - fix width

--- src/sections/UpdatesSection.tsx
+++ src/sections/UpdatesSection.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Button, styled } from '@mui/material'
+import { Box, Typography, Button, styled, Grid } from '@mui/material'
 
 import update1 from '../assets/images/update-1.png'
 import update3 from '../assets/images/update-3.png'
@@ -48,61 +48,65 @@ const UpdateItem = ({
 export const UpdatesSection = () => {
   return (
     <Box py={20}>
-      <Box mb={20}>
-        <Button
-          variant="rounded"
-          sx={{
-            border: `1px solid ${palette.info.main}`,
-            color: palette.info.main,
-          }}
-        >
-          updates
-        </Button>
-      </Box>
-      <Box gap={4} display="flex">
-        <UpdateItemWrapper>
-          <UpdateItem
-            image={update1}
-            date={`JULY 9TH 2022`}
-            title="Atrium 2.0 is coming next month, see what’s to come!"
-          />
-        </UpdateItemWrapper>
-        <UpdateItemWrapper>
-          <UpdateItem
-            image={update5}
-            date="JULY 9TH 2022"
-            title="New meeting points established for newbies"
-            small
-          />
-          <UpdateItem
-            image={update6}
-            date="JULY 9TH 2022"
-            title="ASAC becomes Atrium’s 1st project to reach 10k"
-            small
-          />
-        </UpdateItemWrapper>
-        <UpdateItemWrapper>
-          <UpdateItem
-            image={update3}
-            date={`JULY 9TH 2022`}
-            title="Partnership with Near Big Brain allows for new features"
-          />
-        </UpdateItemWrapper>
-        <UpdateItemWrapper>
-          <UpdateItem
-            image={update7}
-            date="JULY 9TH 2022"
-            title="Haven DAO grows to the largest project on Atrium"
-            small
-          />
-          <UpdateItem
-            image={update8}
-            date="JULY 9TH 2022"
-            title="TAYC skins integrated, see what else is in store!"
-            small
-          />
-        </UpdateItemWrapper>
-      </Box>
+      <Grid container justifyContent="center">
+        <Grid item lg={10}>
+          <Box mb={20}>
+            <Button
+              variant="rounded"
+              sx={{
+                border: `1px solid ${palette.info.main}`,
+                color: palette.info.main,
+              }}
+            >
+              updates
+            </Button>
+          </Box>
+          <Box gap={4} display="flex">
+            <UpdateItemWrapper>
+              <UpdateItem
+                image={update1}
+                date={`JULY 9TH 2022`}
+                title="Atrium 2.0 is coming next month, see what’s to come!"
+              />
+            </UpdateItemWrapper>
+            <UpdateItemWrapper>
+              <UpdateItem
+                image={update5}
+                date="JULY 9TH 2022"
+                title="New meeting points established for newbies"
+                small
+              />
+              <UpdateItem
+                image={update6}
+                date="JULY 9TH 2022"
+                title="ASAC becomes Atrium’s 1st project to reach 10k"
+                small
+              />
+            </UpdateItemWrapper>
+            <UpdateItemWrapper>
+              <UpdateItem
+                image={update3}
+                date={`JULY 9TH 2022`}
+                title="Partnership with Near Big Brain allows for new features"
+              />
+            </UpdateItemWrapper>
+            <UpdateItemWrapper>
+              <UpdateItem
+                image={update7}
+                date="JULY 9TH 2022"
+                title="Haven DAO grows to the largest project on Atrium"
+                small
+              />
+              <UpdateItem
+                image={update8}
+                date="JULY 9TH 2022"
+                title="TAYC skins integrated, see what else is in store!"
+                small
+              />
+            </UpdateItemWrapper>
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
   )
 }
