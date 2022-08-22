53aee49b35ce45ab14f23d66e1bf88869d80e256
chris bishop <ychrisbishop@gmail.com>
2022-08-22 05:30:35 +0200

    Update EndlessIntegrations.tsx

--- src/sections/IntegrationsSection/EndlessIntegrations.tsx
+++ src/sections/IntegrationsSection/EndlessIntegrations.tsx
@@ -1,9 +1,27 @@
-import { Box, Button, Typography, Grid } from '@mui/material'
+import { Box, Typography, Grid } from '@mui/material'
 
-const EndlessIntegrations = () => {
+import EndlessGif from '../../assets/gifs/endless.gif'
+import { palette } from '../../themes/AtriumTheme'
+
+const ProjectIntegrations = () => {
   return (
-    <Box>EndlessIntegrations</Box>
+    <Box>
+      <Grid container justifyContent="center">
+        <Grid item lg={5}>
+          <img src={EndlessGif} alt="" width="100%" />
+        </Grid>
+        <Grid item lg={1} />
+        <Grid item lg={2} sx={{ color: palette.text.primary, pt: 50 }}>
+          <Typography variant="h3">Endless Integrations</Typography>
+          <Typography variant="body2" mt={6}>
+            We’ve started with ASAC, and we are looking to integrate our next 50
+            projects. If you want a central hub inside Atrium, submit your
+            project today!
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
-export default EndlessIntegrations
+export default ProjectIntegrations
