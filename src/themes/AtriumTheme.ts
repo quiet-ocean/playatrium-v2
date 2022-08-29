2952115bd56a56192996f768beb4257e10060cc5
chris bishop <ychrisbishop@gmail.com>
2022-08-29 15:58:57 +0200

    Update AtriumTheme.ts

--- src/themes/AtriumTheme.ts
+++ src/themes/AtriumTheme.ts
@@ -179,8 +179,14 @@ const AtriumTheme = createTheme({
       lineHeight: '14.7px',
       textAlign: 'left',
     },
-    // h1: {
-    // },
+    h1: {
+      fontFamily: 'Fractul Alt',
+      fontSize: '64px',
+      fontWeight: 600,
+      letterSpacing: '-0.03em',
+      lineHeight: '70px',
+      textAlign: 'center',
+    },
     h2: {
       //styleName: Desktop/H2;
       fontFamily: 'Fractul Alt',
