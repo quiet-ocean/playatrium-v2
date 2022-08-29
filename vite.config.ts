e184e1c499c26f088bd3dfabf39438eb34a60290
chris bishop <ychrisbishop@gmail.com>
2022-08-29 05:51:00 +0200

    install vite svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr';
+// import svgLoader from 'vite-svg-loader'
 import { defineConfig } from 'vite'
-
+// import reactSvgPlugin from 'vite-plugin-react-svg'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     sourcemap: true,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      include: 'src/assets/images/*.svg'
+    }),
+    // reactSvgPlugin()
+  ],
   server: {
     host: true,
     open: false,
