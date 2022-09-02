78e8086fcccdac02875f43d903d69ef15947ff6e
chris bishop <ychrisbishop@gmail.com>
2022-09-02 17:08:23 +0200

    Clean files

--- src/sections/OverviewSection.tsx
+++ src/sections/OverviewSection.tsx
@@ -5,6 +5,7 @@ import { useRef, useEffect, useState } from 'react'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Slider from 'react-slick'
+
 import { palette } from '../themes/AtriumTheme'
 
 import { SubtitleText } from './UpdatesSection'
@@ -33,6 +34,7 @@ export const OverviewSection = ({ progress }: { progress: number }) => {
   //   console.log('progress in handle animation function is: ', progress)
   // }, [progress])
   useEffect(() => {
+    console.log(index, done)
     if (index > length && !done) {
       setDone(true)
       sliderRef.current?.slickNext()
@@ -64,54 +66,51 @@ export const OverviewSection = ({ progress }: { progress: number }) => {
     const matches = useMediaQuery(theme.breakpoints.up('md'))
 
     return (
-      <Box>
-        {/* <Box textAlign="center">
-          <SubtitleText color={palette.error.main}>overview</SubtitleText>
-        </Box> */}
-        <Box
-          py={{ md: 30, xs: 16 }}
+      <Box
+        // py={{ md: 30, xs: 16 }}
+        py={{ md: 20, xs: 12 }}
+        sx={{
+          '& *': {
+            color: palette.text.primary,
+          },
+        }}
+      >
+        <Typography
+          variant="h2"
           sx={{
-            '& *': {
-              color: palette.text.primary,
-            },
+            fontSize: { md: 72, xs: 48 },
+            lineHeight: { md: '110%', xs: '120%' },
+            margin: 'auto',
+            maxWidth: 1300,
+            textAlign: { md: 'center', xs: 'left' },
           }}
         >
-          <Typography
-            variant="h2"
-            sx={{
-              fontSize: { md: 72, xs: 48 },
-              lineHeight: { md: '110%', xs: '120%' },
-              margin: 'auto',
-              maxWidth: 1300,
-              textAlign: { md: 'center', xs: 'left' },
-            }}
-          >
-            {text.split('').map((char: string, key: number) => (
-              <span
-                key={key}
-                style={{
-                  color:
-                    key >= startPos && key < endPos
-                      ? theme.palette.error.main
-                      : '',
-                  visibility: matches
-                    ? index > key
-                      ? 'visible'
-                      : 'hidden'
-                    : 'visible',
-                }}
-              >
-                {char}
-              </span>
-            ))}
-          </Typography>
-        </Box>
+          {text.split('').map((char: string, key: number) => (
+            <span
+              key={key}
+              style={{
+                color:
+                  key >= startPos && key < endPos
+                    ? theme.palette.error.main
+                    : '',
+                visibility: matches
+                  ? index > key
+                    ? 'visible'
+                    : 'hidden'
+                  : 'visible',
+              }}
+            >
+              {char}
+            </span>
+          ))}
+        </Typography>
       </Box>
     )
   }
   const Slide2 = () => (
     <Box
-      // py={{ md: 20, xs: 4 }}
+      // py={{ md: 10, xs: 4 }}
+      // py={{ md: 20, xs: 12 }}
       height="100%"
       sx={{
         '& > video': {
@@ -122,7 +121,7 @@ export const OverviewSection = ({ progress }: { progress: number }) => {
       }}
     >
       {/* <ReactPlayer url={`/gamedemo.mp4`} playing={done} width="100%" height="100%" /> */}
-      <video id="video" width="100%" autoPlay={done}>
+      <video id="video" width="100%" autoPlay={true}>
         <track kind="captions" />
         <source src="/gamedemo.mp4" type="video/mp4" />
       </video>
