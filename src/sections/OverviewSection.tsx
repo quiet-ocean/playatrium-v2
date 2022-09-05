66bcb3f814fd360b0f4045b760e78dafd6aae8a2
chris bishop <ychrisbishop@gmail.com>
2022-09-05 16:46:06 +0200

    Fix pass ref variable bug

--- src/sections/OverviewSection.tsx
+++ src/sections/OverviewSection.tsx
@@ -1,7 +1,7 @@
 import { Box, Typography, Grid } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
-import useMediaQuery from '@mui/material/useMediaQuery'
-import { useRef, useEffect, useState } from 'react'
+// import useMediaQuery from '@mui/material/useMediaQuery'
+import { useRef, useEffect, useState, forwardRef } from 'react'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Slider from 'react-slick'
@@ -17,17 +17,15 @@ const length = text.length
 const startPos = text.indexOf('build')
 const endPos = text.indexOf('through')
 
-const OverviewText = ({
-  ref,
-  index,
-  height,
-}: {
-  ref: React.RefObject<HTMLDivElement>
-  index: number
-  height: number
-}) => {
+const OverviewText = forwardRef<
+  HTMLDivElement,
+  {
+    index: number
+    height: number
+  }
+>(({ index, height }, ref) => {
   const theme = useTheme()
-  const matches = useMediaQuery(theme.breakpoints.up('md'))
+  // const matches = useMediaQuery(theme.breakpoints.up('md'))
   return (
     <Box
       ref={ref}
@@ -69,43 +67,39 @@ const OverviewText = ({
       </Typography>
     </Box>
   )
-}
-const OverviewVideo = ({
-  ref,
-  height,
-}: {
-  ref: React.RefObject<HTMLVideoElement>
-  height: number
-}) => {
-  return (
-    <Box
-      py={{ md: 20, xs: 12 }}
-      sx={{
-        '& > video': {
-          borderRadius: { md: '12px', xs: '8px' },
-          // display: `${!done ? 'none' : 'block'}`,
-          height: '100%',
-          objectFit: 'fill',
-        },
-        height: `${height}px`,
-      }}
-      id="video-container"
-    >
-      <video
-        muted
-        ref={ref}
-        preload="none"
-        controls
-        id="video"
-        width="100%"
-        autoPlay
+})
+const OverviewVideo = forwardRef<HTMLVideoElement, { height: number }>(
+  ({ height }, ref) => {
+    return (
+      <Box
+        py={{ md: 20, xs: 12 }}
+        sx={{
+          '& > video': {
+            borderRadius: { md: '12px', xs: '8px' },
+            // display: `${!done ? 'none' : 'block'}`,
+            height: '100%',
+            objectFit: 'fill',
+          },
+          height: `${height}px`,
+        }}
+        id="video-container"
       >
-        <track kind="captions" />
-        <source src="/gamedemo.mp4" type="video/mp4" />
-      </video>
-    </Box>
-  )
-}
+        <video
+          muted
+          ref={ref}
+          preload="none"
+          controls
+          id="video"
+          width="100%"
+          autoPlay
+        >
+          <track kind="captions" />
+          <source src="/gamedemo.mp4" type="video/mp4" />
+        </video>
+      </Box>
+    )
+  }
+)
 // const MobileContent = () => {
 
 // }
