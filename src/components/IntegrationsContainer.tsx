4cbe0496e7f89e71d116021ed5de9f610e69fc92
chris bishop <ychrisbishop@gmail.com>
2022-09-06 17:41:03 +0200

    Update IntegrationsContainer.tsx

--- src/components/IntegrationsContainer.tsx
+++ src/components/IntegrationsContainer.tsx
@@ -1,10 +1,17 @@
 import { Box, styled } from '@mui/material'
 
-export const SIntegrationsContainer = styled(Box)(({ theme }) => ({
+export const IntegrationsContainer = styled(Box)(({ theme }) => ({
   '& div.endless-panel': {
     background: theme.palette.common.black,
     position: 'absolute',
     top: '100%',
+  },
+  '& div.screen-panel': {
+    background: theme.palette.common.black,
+    height: '100%',
+    position: 'absolute',
+    top: '100%',
+    width: '100%',
   },
   '&.done div.endless-panel': {
     top: '0% !important',
@@ -15,30 +22,3 @@ export const SIntegrationsContainer = styled(Box)(({ theme }) => ({
   position: 'relative',
   width: '100%',
 }))
-
-export const IntegrationsContainer = ({
-  children,
-  done,
-}: {
-  children: React.ReactNode
-  done: boolean
-}) => {
-  return (
-    <SIntegrationsContainer
-      className={`${done ? 'done' : ''}`}
-      sx={
-        {
-          // '& div.endless-panel': {
-          //   position: done ? 'static' : 'absolute',
-          // top: done ? '0% !important' : '',
-          // },
-          // '& div.project-panel': {
-          //   display: done ? 'none' : 'block',
-          // },
-        }
-      }
-    >
-      {children}
-    </SIntegrationsContainer>
-  )
-}
